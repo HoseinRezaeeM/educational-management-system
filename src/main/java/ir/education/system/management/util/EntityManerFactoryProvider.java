96fc7f717608201ca0663f54d82f5496ac395619
HoseinRezaei <rezaeen77@gmail.com>
2023-11-26 18:50:21 +0330

    create EntityMangerFactory

---
+++ src/main/java/ir/education/system/management/util/EntityManerFactoryProvider.java
@@ -0,0 +1,17 @@
+package ir.education.system.management.util;
+
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManerFactoryProvider {
+    private final static EntityManagerFactory entityManagerFactory;
+
+    static{
+        entityManagerFactory = Persistence.createEntityManagerFactory("default");
+
+    }
+    public static EntityManagerFactory getEntityManagerFactory(){
+        return entityManagerFactory;
+    }
+}
