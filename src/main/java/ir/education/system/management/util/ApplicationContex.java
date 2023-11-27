68fd4fc7515501e257f1beafa8f43139733ac1e0
HoseinRezaei <rezaeen77@gmail.com>
2023-11-27 15:41:12 +0330

    Write ApplicationContex for create object

---
+++ src/main/java/ir/education/system/management/util/ApplicationContex.java
@@ -0,0 +1,67 @@
+package ir.education.system.management.util;
+
+import ir.education.system.management.repository.*;
+import ir.education.system.management.repository.impl.*;
+import ir.education.system.management.service.*;
+import ir.education.system.management.service.impl.*;
+
+import javax.persistence.EntityManager;
+
+public class ApplicationContex {
+
+    private final static EntityManager entityManager;
+    private static final EmployeeRepository employeeRepository;
+    private static final EmployeeService employeeService;
+    private static final ProfessorRepository professorRepository;
+    private static final ProfessorService professorService;
+    private static final CourseRepository courseRepository;
+    private static final CourseService courseService;
+    private static final StudentRepository studentRepository;
+    private static final StudentService studentService;
+    private static final SelectedCourseRepository selectedCourseRepository;
+    private static final SelectedCourseSrevice selectedCourseSrevice;
+    private static final SemesterProfessorRepository semesterProfessorRepository;
+    private static final SemesterProfessorService semesterProfessorService;
+
+    static {
+        entityManager = EntityManerFactoryProvider.getEntityManagerFactory().createEntityManager();
+        employeeRepository = new EmployeeRepositoryImpl(entityManager);
+        employeeService = new EmployeeServiceImpl(employeeRepository);
+        professorRepository = new ProfessorRepositoryImpl(entityManager);
+        professorService = new ProfessorServiceImpl(professorRepository);
+        courseRepository = new CourseRepositoryImpl(entityManager);
+        courseService = new CourseServiceImpl(courseRepository);
+        studentRepository = new StudentRepositoryImpl(entityManager);
+        studentService = new StudentServiceImpl(studentRepository);
+        selectedCourseRepository = new SelectedCourseRepositoryImpl(entityManager);
+        selectedCourseSrevice = new SelectedCourseServiceImpl(selectedCourseRepository);
+        semesterProfessorRepository = new SemesterProfessorRepositoryImpl(entityManager);
+        semesterProfessorService = new SemesterProfessorSeriveImpl(semesterProfessorRepository);
+
+    }
+
+    public static EmployeeService getEmployeeServiceImpl() {
+        return employeeService;
+    }
+
+    public static ProfessorService getProfessorServiceImpl() {
+        return professorService;
+    }
+
+    public static CourseService getCourseServiceImpl() {
+        return courseService;
+    }
+
+    public static SelectedCourseSrevice getSelectedCourseSreviceImpl() {
+        return selectedCourseSrevice;
+    }
+
+    public static SemesterProfessorService getSemesterProfessorServiceImpl() {
+        return semesterProfessorService;
+    }
+
+    public static StudentService getStudentServiceImpl() {
+        return studentService;
+    }
+
+}
