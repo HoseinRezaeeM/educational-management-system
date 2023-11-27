4101658a1a41df7cee5417c5df48671b5023f07f
HoseinRezaei <rezaeen77@gmail.com>
2023-11-27 15:02:33 +0330

    Write CRUD opertion Impl

---
+++ src/main/java/ir/education/system/management/base/repository/impl/BaseEntityRepositoryImpl.java
@@ -0,0 +1,75 @@
+package ir.education.system.management.base.repository.impl;
+
+import ir.education.system.management.base.domin.BaseEntity;
+import ir.education.system.management.base.repository.BaseEntityRepository;
+
+import javax.persistence.EntityManager;
+import java.io.Serializable;
+import java.util.Collection;
+@SuppressWarnings("unused")
+public abstract class BaseEntityRepositoryImpl<ID extends Serializable, T extends BaseEntity<ID>> implements BaseEntityRepository<ID, T> {
+    protected EntityManager entityManager;
+
+    public BaseEntityRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public T save(T entity) {
+        beginTransaction();
+        entity = saveWithoutTransaction(entity);
+        commitTransaction();
+        return entity;
+    }
+
+    private T saveWithoutTransaction(T entity) {
+        if (entity.getId() == null)
+            entityManager.persist(entity);
+        else
+            entity = entityManager.merge(entity);
+        return entity;
+    }
+
+    @Override
+    public T update(T entity, ID id) {
+        beginTransaction();
+        entity.setId(id);
+        T merge = entityManager.merge(entity);
+        commitTransaction();
+        entityManager.clear();
+        return merge;
+    }
+
+    @Override
+    public void deleteById(ID id) {
+        beginTransaction();
+        entityManager.remove(id);
+        commitTransaction();
+    }
+
+    @Override
+    public Collection<T> findAll() {
+        return entityManager.createQuery(
+                "from " + getEntityClass().getSimpleName(), getEntityClass()).getResultList();
+    }
+
+    public abstract Class<T> getEntityClass();
+
+    @Override
+    public void beginTransaction() {
+        if (!entityManager.getTransaction().isActive())
+            entityManager.getTransaction().begin();
+    }
+
+    @Override
+    public void commitTransaction() {
+        if (entityManager.getTransaction().isActive())
+            entityManager.getTransaction().commit();
+    }
+
+    @Override
+    public void rollBack() {
+        if (entityManager.getTransaction().isActive())
+            entityManager.getTransaction().rollback();
+    }
+}
