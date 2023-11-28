2375febb39f93f77f0332029ac66118c934bf48a
HoseinRezaei <rezaeen77@gmail.com>
2023-11-28 17:16:28 +0330

    create UserService

---
+++ src/main/java/ir/education/system/management/service/UserService.java
@@ -0,0 +1,7 @@
+package ir.education.system.management.service;
+
+import ir.education.system.management.domin.base.BaseUser;
+import ir.education.system.management.service.base.BaseUserService;
+
+public interface UserService extends BaseUserService<BaseUser> {
+}
