1d8f26de65f9a19bc48d76c981045f049a69db3a
HoseinRezaei <rezaeen77@gmail.com>
2023-11-28 17:04:34 +0330

    create UserRepository interface

---
+++ src/main/java/ir/education/system/management/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package ir.education.system.management.repository;
+
+import ir.education.system.management.domin.base.BaseUser;
+import ir.education.system.management.repository.base.BaseUserRepository;
+
+public interface UserRepository extends BaseUserRepository<BaseUser> {
+}
