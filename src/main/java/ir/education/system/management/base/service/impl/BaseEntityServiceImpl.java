ae33170124920d63d3069c5d6e0b536086d9a83b
HoseinRezaei <rezaeen77@gmail.com>
2023-11-29 14:59:32 +0330

    Create BaseEntityServiceImpl abstract

---
+++ src/main/java/ir/education/system/management/base/service/impl/BaseEntityServiceImpl.java
@@ -0,0 +1,58 @@
+package ir.education.system.management.base.service.impl;
+
+import ir.education.system.management.base.domin.BaseEntity;
+import ir.education.system.management.base.repository.BaseEntityRepository;
+import ir.education.system.management.base.service.BaseEntityService;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Optional;
+
+public class BaseEntityServiceImpl<ID extends Serializable, T extends BaseEntity<ID>, R extends BaseEntityRepository<ID, T>> implements BaseEntityService<ID, T> {
+
+    protected final R baseRepository;
+
+    public BaseEntityServiceImpl(R baseRepository) {
+        this.baseRepository = baseRepository;
+    }
+
+    @Override
+    public T save(T entity) {
+        return baseRepository.save(entity);
+    }
+
+    @Override
+    public T update(T entity, ID id) {
+        return baseRepository.update(entity, id);
+    }
+
+    @Override
+    public void deleteById(ID id) {
+        baseRepository.deleteById(id);
+    }
+
+    @Override
+    public Collection<T> findAll() {
+        return baseRepository.findAll();
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        return baseRepository.findById(id);
+    }
+
+    @Override
+    public void beginTransaction() {
+        baseRepository.beginTransaction();
+    }
+
+    @Override
+    public void commitTransaction() {
+        baseRepository.commitTransaction();
+    }
+
+    @Override
+    public void rollBack() {
+        baseRepository.rollBack();
+    }
+}
