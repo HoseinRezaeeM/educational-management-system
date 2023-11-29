343409a81e3a63642b2d24a5dd64961ac9092f59
HoseinRezaei <rezaeen77@gmail.com>
2023-11-29 22:15:47 +0330

    write regex validation for password email mobilenumber

---
+++ src/main/java/ir/education/system/management/validation/RegexValidation.java
@@ -0,0 +1,20 @@
+package ir.education.system.management.validation;
+
+import java.util.regex.Pattern;
+
+public class RegexValidation {
+
+        public static boolean validatonPassword(String password){
+            Pattern pattern =Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
+            return password.matches(pattern.pattern());
+        }
+        public static boolean validaitonEmail(String email){
+            Pattern pattern =Pattern.compile("[a-z0-9]+@[a-z]+\\.[a-z]{2,3}");
+            return email.matches(pattern.pattern());
+        }
+        public static boolean validationMobileNumber(String mobileNumber){
+            Pattern pattern =Pattern.compile("^[0-9]{11}$");
+            return mobileNumber.matches(pattern.pattern());
+        }
+    }
+
