f04a3358044f085f08adfe180b1842d4a8608869
HoseinRezaei <rezaeen77@gmail.com>
2023-11-29 21:57:17 +0330

    Write test crud method

---
+++ src/test/java/baserepositorytest/BaseEntityRepositoryImplTest.java
@@ -0,0 +1,59 @@
+package baserepositorytest;
+
+import ir.education.system.management.domin.Employee;
+import ir.education.system.management.util.ApplicationContex;
+import ir.education.system.management.util.EntityManerFactoryProvider;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.function.BooleanSupplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseEntityRepositoryImplTest {
+
+    EntityManager entityManager;
+
+    @BeforeEach
+    void createObject() {
+        entityManager = EntityManerFactoryProvider.getEntityManagerFactory().createEntityManager();
+    }
+
+    @Test
+    void save() {
+        Employee employee = new Employee("Hosein", "Rezaei", 25, "HRRB", "QAa1@234", "rezaeen77@emial.com", "09125174512", 12000000L, "1401");
+        ApplicationContex.getEmployeeServiceImpl().save(employee);
+        boolean contains = ApplicationContex.getEmployeeServiceImpl().existsByUsername("HRRB");
+        Assertions
+                .assertTrue(contains);
+    }
+
+    @Test
+    void update() {
+        Employee employee = new Employee();
+        Employee newEmployee =new Employee("Hassan", "Rezaei", 25, "HRRB", "QAa1@234", "rezaeen77@emial.com", "09125174512", 12000000L, "1401");
+        newEmployee.setFirstname("Hassan");
+        ApplicationContex.getEmployeeServiceImpl().update(newEmployee,4);
+        final Optional<Employee> optionalEmployee = ApplicationContex.getEmployeeServiceImpl().findById(4);
+        Assertions
+                .assertEquals(newEmployee.getFirstname(),optionalEmployee.get().getFirstname());
+    }
+
+    @Test
+    void deleteById() {
+        ApplicationContex.getEmployeeServiceImpl().deleteById(3);
+        final Optional<Employee> optionalEmployee = ApplicationContex.getEmployeeServiceImpl().findById(3);
+        Assertions.assertTrue(optionalEmployee.isEmpty());
+    }
+
+    @Test
+    void findAll() {
+        final Collection<Employee> all = ApplicationContex.getEmployeeServiceImpl().findAll();
+        Assertions.assertNotNull(all);
+    }
+}
