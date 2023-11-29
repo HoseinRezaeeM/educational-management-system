59f748f68e7211e79440e08b0451fe96fa02ca92
HoseinRezaei <rezaeen77@gmail.com>
2023-11-29 14:59:40 +0330

    Create BaseEntityService

--- src/main/java/ir/education/system/management/base/service/BaseEntityService.java
+++ src/main/java/ir/education/system/management/base/service/BaseEntityService.java
@@ -5,6 +5,7 @@ import ir.education.system.management.base.domin.BaseEntity;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.Optional;
 
 public interface BaseEntityService<ID extends Serializable,T extends BaseEntity<ID>> {
     T save(T entity);
@@ -15,6 +16,7 @@ public interface BaseEntityService<ID extends Serializable,T extends BaseEntity<
 
     Collection<T> findAll();
 
+    Optional<T> findById(ID id);
     void beginTransaction();
 
     void commitTransaction();
