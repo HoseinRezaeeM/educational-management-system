698fb95e52bcee14801c61fe11fccec54f046bbf
HoseinRezaei <rezaeen77@gmail.com>
2023-11-30 17:33:07 +0330

    create StudentService

---
+++ src/main/java/ir/education/system/management/service/StudentService.java
@@ -0,0 +1,7 @@
+package ir.education.system.management.service;
+
+import ir.education.system.management.domin.Student;
+import ir.education.system.management.service.base.BaseUserService;
+
+public interface StudentService extends BaseUserService<Student> {
+}
