81f0b88a64608747def9eed4f5d167f4bdf5d795
HoseinRezaei <rezaeen77@gmail.com>
2023-11-30 17:32:51 +0330

    create StudentRepository

---
+++ src/main/java/ir/education/system/management/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package ir.education.system.management.repository;
+
+import ir.education.system.management.base.repository.BaseEntityRepository;
+import ir.education.system.management.domin.Student;
+import ir.education.system.management.repository.base.BaseUserRepository;
+
+public interface StudentRepository extends BaseUserRepository<Student> {
+}
