fea87b11718f80abefb1e01b61c9fec379030b63
HoseinRezaei <rezaeen77@gmail.com>
2023-11-30 14:44:26 +0330

    Create ProfessorService

---
+++ src/main/java/ir/education/system/management/service/ProfessorService.java
@@ -0,0 +1,8 @@
+package ir.education.system.management.service;
+
+import ir.education.system.management.base.service.BaseEntityService;
+import ir.education.system.management.domin.Professor;
+import ir.education.system.management.service.base.BaseUserService;
+
+public interface ProfessorService extends BaseUserService< Professor> {
+}
