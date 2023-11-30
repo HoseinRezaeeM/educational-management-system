024f36edae661673c126104d10048b2f58d84d2a
HoseinRezaei <rezaeen77@gmail.com>
2023-11-30 17:21:17 +0330

    edit CourseRepository

---
+++ src/main/java/ir/education/system/management/repository/CourseRepository.java
@@ -0,0 +1,7 @@
+package ir.education.system.management.repository;
+
+import ir.education.system.management.base.repository.BaseEntityRepository;
+import ir.education.system.management.domin.Course;
+
+public interface CourseRepository extends BaseEntityRepository<Integer, Course> {
+}
