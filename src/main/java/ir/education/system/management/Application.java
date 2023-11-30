f80bf41be1c82e7a730ee8b6a74a9770224698c8
HoseinRezaei <rezaeen77@gmail.com>
2023-11-30 17:46:06 +0330

    main class

---
+++ src/main/java/ir/education/system/management/Application.java
@@ -0,0 +1,16 @@
+package ir.education.system.management;
+
+
+import ir.education.system.management.menu.BaseMenu;
+import java.sql.SQLException;
+
+public class Application {
+    public static void main(String[] args) throws SQLException {
+       new BaseMenu().fistMenu();
+
+//Develope By Hosein Rezaei
+
+    }
+
+
+}
