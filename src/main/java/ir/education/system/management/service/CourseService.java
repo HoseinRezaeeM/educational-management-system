98a0532cc1497ff6d1c568fd850c3899cbdced14
HoseinRezaei <rezaeen77@gmail.com>
2023-11-30 17:21:36 +0330

    edit CourseService

---
+++ src/main/java/ir/education/system/management/service/CourseService.java
@@ -0,0 +1,7 @@
+package ir.education.system.management.service;
+
+import ir.education.system.management.base.service.BaseEntityService;
+import ir.education.system.management.domin.Course;
+
+public interface CourseService extends BaseEntityService<Integer, Course> {
+}
