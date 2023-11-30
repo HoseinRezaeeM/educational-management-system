4447e6510a57434da8cadd4fecce16daaf47b64e
HoseinRezaei <rezaeen77@gmail.com>
2023-11-30 17:19:59 +0330

    Create EmployeeService

---
+++ src/main/java/ir/education/system/management/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package ir.education.system.management.service;
+
+import ir.education.system.management.base.service.BaseEntityService;
+import ir.education.system.management.domin.Employee;
+import ir.education.system.management.service.base.BaseUserService;
+
+import java.beans.IntrospectionException;
+
+public interface EmployeeService extends BaseUserService< Employee> {
+}
