3482d520b54d7ac9e379f85a9d7d5b6239943c63
HoseinRezaei <rezaeen77@gmail.com>
2023-11-30 17:19:19 +0330

    Create EmployeeRepository

---
+++ src/main/java/ir/education/system/management/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package ir.education.system.management.repository;
+
+import ir.education.system.management.base.repository.BaseEntityRepository;
+import ir.education.system.management.domin.Employee;
+import ir.education.system.management.repository.base.BaseUserRepository;
+
+public interface EmployeeRepository extends BaseUserRepository<Employee> {
+}
